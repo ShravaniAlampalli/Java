5f08a603b5e9d4944c1d8e6887c2fbe5b196bb63
Shravani Alampalli <alampallishravani@gmail.com>
2017-09-13 18:01:37 -0500

    initial commit

---
+++ Desktop/Alampalli/src/PrintNameBox.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shravani Alampalli
+ */
+public class PrintNameBox {
+    
+    public static void main(String args[]){
+        System.out.println("+----------+");
+        System.out.println("| Shravani |");
+        System.out.println("+----------+");
+    }
+    
+}
